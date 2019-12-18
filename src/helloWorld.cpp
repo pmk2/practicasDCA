b255d4dc3a2eac1530403604758187370e004b5b
pmk2 <pmk2@alu.ua.es>
2019-12-18 19:59:07 +0100

    Anyadida funcionalidad de sumar 3 + 3

--- src/helloWorld.cpp
+++ src/helloWorld.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 
+int suma(int a, int b) {
+        int resul = 0;
+        resul = a + b;
+        return resul;
+}
+
 int main() {
 	cout << "Hello World!" << endl;
+	cout << "Probando la suma de 3 + 3 = " << suma(3,3) << endl; 
 	return 0;
 }
