89f97dc635cb2de465ac2e7d54c60f6b20bb50b3
pmk2 <pmk2@alu.ua.es>
2019-12-18 20:09:58 +0100

    Anyadida funcionalidad multilpicacion 5 * 2

--- src/helloWorld.cpp
+++ src/helloWorld.cpp
@@ -20,10 +20,18 @@ int division(int a, int b) {
         return resul;
 }
 
+int multiplicacion(int a, int b) {
+        int resul = 0;
+        resul = a * b;
+        return resul;
+}
+
 int main() {
 	cout << "Hello World!" << endl;
 	cout << "Probando la suma de 3 + 3 = " << suma(3,3) << endl; 
 	cout << "Probando la resta de 5 - 2 = " << resta(5,2) << endl;
 	cout << "Probando la division de 5 / 2 = " << division(6,2) << endl;
+	cout << "Probando la multiplicacion de 5 * 2 = " << multiplicacion(5,2) << endl;
 	return 0;
 }
+
