5114e41d661590229c1cb2029c495d9300e7157f
pmk2 <pmk2@alu.ua.es>
2019-12-18 20:02:30 +0100

    Anyadida resta de 5 - 2

--- src/helloWorld.cpp
+++ src/helloWorld.cpp
@@ -4,12 +4,19 @@ using namespace std;
 
 int suma(int a, int b) {
         int resul = 0;
-        resul = a + b;
+        resul = 5;
+        return resul;
+}
+
+int resta(int a, int b) {
+        int resul = 0;
+        resul = a - b;
         return resul;
 }
 
 int main() {
 	cout << "Hello World!" << endl;
 	cout << "Probando la suma de 3 + 3 = " << suma(3,3) << endl; 
+	cout << "Probando la resta de 5 - 2 = " << resta(5,2) << endl;
 	return 0;
 }
