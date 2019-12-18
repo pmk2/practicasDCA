2b7e98d7de4b198cc4e23c55b471177c691aca23
pmk2 <pmk2@alu.ua.es>
2019-12-18 20:07:13 +0100

    Anyadida funcionalidad de division 6 / 2

--- src/helloWorld.cpp
+++ src/helloWorld.cpp
@@ -14,9 +14,16 @@ int resta(int a, int b) {
         return resul;
 }
 
+int division(int a, int b) {
+        int resul = 0;
+        resul = a / b;
+        return resul;
+}
+
 int main() {
 	cout << "Hello World!" << endl;
 	cout << "Probando la suma de 3 + 3 = " << suma(3,3) << endl; 
 	cout << "Probando la resta de 5 - 2 = " << resta(5,2) << endl;
+	cout << "Probando la division de 5 / 2 = " << division(6,2) << endl;
 	return 0;
 }
